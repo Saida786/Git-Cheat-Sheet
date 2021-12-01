471826e1ca29241fd0904f004c034af9f78f64ad
saida <saida@gmail.com>
2021-12-01 11:34:52 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,35 @@
+data = [
+    {
+        'id': 'one',
+        'command': 'c:\>cd'
+    },
+
+    {
+        'id': 'two',
+        'command': 'c:\>git init'
+    },
+
+
+    {
+        'id': 'three',
+        'command': 'c:\>dir '
+    },
+
+
+    {
+        'id': 'four',
+        'command': 'c:\>dir /a'
+    },
+
+
+    {
+        'id': 'five',
+        'command': 'c:\>git config --global user.name "Saida"'
+    },
+
+
+    {
+        'id': 'six',
+        'command': 'c:\>git config --global user.email "saida@gmail.com"'
+    }
+]
